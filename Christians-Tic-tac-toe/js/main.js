7d5ab93c98e0c31cf6ddc843e6de8224fa310023
Chrissszz <c.barrett228@yahoo.com>
2023-03-31 15:35:58 -0500

    added some js and pictures

--- Christians-Tic-tac-toe/js/main.js
+++ Christians-Tic-tac-toe/js/main.js
@@ -1,1 +1,8 @@
-//write tic tac toe
+
+//select the class of square and add a click event so when it is clicked it shows picture to be used as X or O
+document.querySelector(".square").addEventListener('click', insertImage)
+//write the function that inserts the image into the html and add a class to be edited in css
+function insertImage(){
+    let shape = document.querySelector(".square");
+    shape.innerHTML = "<img src='images/favpng_jupiter-planet-mercury-uranus-clip-art.png' class='image-class'>";
+}
