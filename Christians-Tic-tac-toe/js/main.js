7d8ed2d390fe2c0f10ed08f0209b6fedb2d37993
Chrissszz <c.barrett228@yahoo.com>
2023-04-02 15:53:52 -0500

    simplified js

--- Christians-Tic-tac-toe/js/main.js
+++ Christians-Tic-tac-toe/js/main.js
@@ -1,119 +1,196 @@
 
-// create an empty array for the empty squares
-var board = ["", "", "", "", "", "", "", "", ""];
-//create a variable for the starting player and keep track of starting player
-var currentPlayer = "X";
-//select all element from the html where the shapes/images will go
-var squares = document.querySelectorAll('.square');
-//loop through each element in squares array, for each square an event listernet is added for a click event
-squares.forEach(function(square) {
-  square.addEventListener('click', function() {
-    //array.from creats a new shallow-copied array then indexOf returns the first index found in the array
-    var index = Array.from(squares).indexOf(square);
-    //if square is occupied by player do nothing otherwise set the board to the current players value x or o depending whos turn it is
-    if (board[index] === "") {
-      board[index] = currentPlayer;
-      // switch players for each turn
-      currentPlayer = currentPlayer === "X" ? "O" : "X";
-      // check for winner
-      if (checkWinner(board)) {
-        alert(currentPlayer + " wins!");
-        // reset game
-        board = ["", "", "", "", "", "", "", "", ""];
-        currentPlayer = "X";
-      } else if (checkTie(board)) {
-        alert("Tie game!");
-        // reset game
-        board = ["", "", "", "", "", "", "", "", ""];
-        currentPlayer = "X";
-      } else {
-        // switch to AI player's turn
-        currentPlayer = "O";
-        var aiIndex = aiMove(board);
-        board[aiIndex] = currentPlayer;
-        // check for winner
-        if (checkWinner(board)) {
-          alert(currentPlayer + " wins!");
-          // reset game
-          board = ["", "", "", "", "", "", "", "", ""];
-          currentPlayer = "X";
-        } else if (checkTie(board)) {
-          alert("Tie game!");
-          // reset game
-          board = ["", "", "", "", "", "", "", "", ""];
-          currentPlayer = "X";
-        } else {
-          // switch back to human player's turn
-          currentPlayer = "X";
-        }
-        // display updated board
-        displayBoard(board);
-      }
-    }
-  });
+// // create an empty array for the empty squares
+// var board = ["", "", "", "", "", "", "", "", ""];
+// //create a variable for the starting player and keep track of starting player
+// var currentPlayer = "X";
+// //select all element from the html where the shapes/images will go
+// var squares = document.querySelectorAll('.square');
+// //loop through each element in squares array, for each square an event listernet is added for a click event
+// squares.forEach(function(square) {
+//   square.addEventListener('click', function() {
+//     //array.from creats a new shallow-copied array then indexOf returns the first index found in the array
+//     var index = Array.from(squares).indexOf(square);
+//     //if square is occupied by player do nothing otherwise set the board to the current players value x or o depending whos turn it is
+//     if (board[index] === "") {
+//       board[index] = currentPlayer;
+//       // switch players for each turn
+//       currentPlayer = currentPlayer === "X" ? "O" : "X";
+//       // check for winner
+//       if (checkWinner(board)) {
+//         alert(currentPlayer + " wins!");
+//         // reset game
+//         board = ["", "", "", "", "", "", "", "", ""];
+//         currentPlayer = "X";
+//       } else if (checkTie(board)) {
+//         alert("Tie game!");
+//         // reset game
+//         board = ["", "", "", "", "", "", "", "", ""];
+//         currentPlayer = "X";
+//       } else {
+//         // switch to AI player's turn
+//         currentPlayer = "O";
+//         var aiIndex = aiMove(board);
+//         board[aiIndex] = currentPlayer;
+//         // check for winner
+//         if (checkWinner(board)) {
+//           alert(currentPlayer + " wins!");
+//           // reset game
+//           board = ["", "", "", "", "", "", "", "", ""];
+//           currentPlayer = "X";
+//         } else if (checkTie(board)) {
+//           alert("Tie game!");
+//           // reset game
+//           board = ["", "", "", "", "", "", "", "", ""];
+//           currentPlayer = "X";
+//         } else {
+//           // switch back to human player's turn
+//           currentPlayer = "X";
+//         }
+//         // display updated board
+//         displayBoard(board);
+//       }
+//     }
+//   });
+// });
+
+// function checkWinner(board) {
+// }
+// function checkTie(board) {
+//   // same as before
+// }
+// function displayBoard(board) {
+//   squares.forEach(function(square, index) {
+//     var image = square.querySelector("img");
+//     if (board[index] === "X") {
+//       image.src = "images/favpng_planet-euclidean-vector.png";
+//     } else if (board[index] === "O") {
+//       image.src = "images/favpng_jupiter-planet-mercury-uranus-clip-art.png";
+//     } else {
+//       image.src = "";
+//     }
+//   });
+// }
+// function aiMove(board) {
+//     // check for winning moves for the player
+//     for (var i = 0; i < board.length; i++) {
+//       if (board[i] === "") {
+//         board[i] = "X";
+//         if (checkWinner(board)) {
+//           board[i] = "O";
+//           return i;
+//         }
+//         board[i] = "";
+//       }
+//     }
+  
+//     // check for winning moves for the AI
+//     for (var i = 0; i < board.length; i++) {
+//       if (board[i] === "") {
+//         board[i] = "O";
+//         if (checkWinner(board)) {
+//           board[i] = "O";
+//           return i;
+//         }
+//         board[i] = "";
+//       }
+//     }
+  
+//     // if no winning moves are available, block the player's moves
+//     var corners = [0, 2, 6, 8];
+//     var sides = [1, 3, 5, 7];
+//     var emptyCorners = corners.filter(i => board[i] === "");
+//     var emptySides = sides.filter(i => board[i] === "");
+  
+//     if (emptyCorners.length > 0) {
+//       return emptyCorners[Math.floor(Math.random() * emptyCorners.length)];
+//     } else if (board[4] === "") {
+//       return 4;
+//     } else if (emptySides.length > 0) {
+//       return emptySides[Math.floor(Math.random() * emptySides.length)];
+//     }
+//   }
+const squares = document.querySelectorAll(".square");
+const playerX = new Image();
+playerX.src = "images/favpng_jupiter-planet-mercury-uranus-clip-art.png"; // Replace with your X image source
+const playerO = new Image();
+playerO.src = "images/favpng_planet-euclidean-vector.png"; // Replace with your O image source
+let currentPlayer = playerX; // Start with player X
+let gameIsOver = false; // Set to true when game is over
+
+const playerOutcome = document.querySelector(".playerOutcome"); // Select the h2 tag
+
+squares.forEach((square) => {
+  square.addEventListener("click", handleClick);
 });
 
-function checkWinner(board) {
+function handleClick(event) {
+  const square = event.target;
+  const index = square.getAttribute("id");
+
+  if (square.innerHTML !== "" || gameIsOver) {
+    return;
+  }
+
+  square.appendChild(currentPlayer.cloneNode()); // Append the Image element
+
+  if (checkForWinner()) {
+    const winnerName = currentPlayer === playerX ? "Uranus" : "Saturn";
+    const outcomeMessage = `${winnerName} wins!`;
+    playerOutcome.textContent = outcomeMessage; // Update the h2 tag with the outcome message
+    gameIsOver = true; // Game is over
+    return;
+  }
+
+  if (checkForTie()) {
+    const outcomeMessage = "It's a tie!";
+    playerOutcome.textContent = outcomeMessage; // Update the h2 tag with the outcome message
+    gameIsOver = true; // Game is over
+    return;
+  }
+
+  currentPlayer = currentPlayer === playerX ? playerO : playerX; // Switch players
 }
-function checkTie(board) {
-  // same as before
-}
-function displayBoard(board) {
-  squares.forEach(function(square, index) {
-    var image = square.querySelector("img");
-    if (board[index] === "X") {
-      image.src = "images/favpng_planet-euclidean-vector.png";
-    } else if (board[index] === "O") {
-      image.src = "images/favpng_jupiter-planet-mercury-uranus-clip-art.png";
-    } else {
-      image.src = "";
-    }
+
+function checkForWinner() {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  return winningCombinations.some((combination) => {
+    return (
+      squares[combination[0]].innerHTML === currentPlayer.outerHTML &&
+      squares[combination[1]].innerHTML === currentPlayer.outerHTML &&
+      squares[combination[2]].innerHTML === currentPlayer.outerHTML
+    );
   });
+}
+
+function checkForTie() {
+  return Array.from(squares).every((square) => {
+    return square.innerHTML !== "";
+  });
+}
+
+function resetGame() {
+  squares.forEach((square) => {
+    square.innerHTML = "";
+  });
+
+  currentPlayer = playerX;
+  playerOutcome.textContent = ""; // Clear the outcome message
+  gameIsOver = false; // Reset game status
 }
 
 document.querySelector(".button").addEventListener("click", playAgain);
 
-function playAgain(){
-    location.reload();
-}
-  
-function aiMove(board) {
-    // check for winning moves for the player
-    for (var i = 0; i < board.length; i++) {
-      if (board[i] === "") {
-        board[i] = "X";
-        if (checkWinner(board)) {
-          board[i] = "O";
-          return i;
-        }
-        board[i] = "";
-      }
-    }
-  
-    // check for winning moves for the AI
-    for (var i = 0; i < board.length; i++) {
-      if (board[i] === "") {
-        board[i] = "O";
-        if (checkWinner(board)) {
-          board[i] = "O";
-          return i;
-        }
-        board[i] = "";
-      }
-    }
-  
-    // if no winning moves are available, block the player's moves
-    var corners = [0, 2, 6, 8];
-    var sides = [1, 3, 5, 7];
-    var emptyCorners = corners.filter(i => board[i] === "");
-    var emptySides = sides.filter(i => board[i] === "");
-  
-    if (emptyCorners.length > 0) {
-      return emptyCorners[Math.floor(Math.random() * emptyCorners.length)];
-    } else if (board[4] === "") {
-      return 4;
-    } else if (emptySides.length > 0) {
-      return emptySides[Math.floor(Math.random() * emptySides.length)];
-    }
-  }
+ function playAgain(){
+     location.reload();
+ }
   
